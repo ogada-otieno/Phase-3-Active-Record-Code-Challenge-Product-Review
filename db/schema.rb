9e9714f5d95102422431a1c0356b9eebcc4dbbdd
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-27 05:07:57 +0300

    Created reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_27_020121) do
+ActiveRecord::Schema.define(version: 2023_02_27_020436) do
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
     t.integer "price", default: 0, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "star_rating"
+    t.string "comment"
+    t.integer "user_id"
+    t.integer "product_id"
   end
 
   create_table "users", force: :cascade do |t|
