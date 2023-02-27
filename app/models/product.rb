07553ad68a833cef2d0564f6eac058c4e84341d4
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-27 09:59:00 +0300

    #leave_review method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
    has_many :reviews
    has_many :users, through: :reviews
+
+   # creates a new Review in the database associated with this Product and the User
+   def leave_review(user, star_rating, comment)
+        Review.create(user_id: user.id, star_rating: star_rating, comment: comment)
+    end
+    
 end
